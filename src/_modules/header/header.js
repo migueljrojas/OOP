f1b172ed07b6630fe02b4937266ab91e63819e18
Mike <migueljrojas@gmail.com>
2020-05-05 16:51:40 -0300

    First content run

--- src/_modules/header/header.js
+++ src/_modules/header/header.js
@@ -1,8 +1,28 @@
 'use strict';
+import $ from 'jquery';
 
 export default class Header {
   constructor() {
-    this.name = 'header';
-    console.log('%s module', this.name.toLowerCase());
+    const indexitems = $('[data-target]');
+    const sections = $('[data-content]');
+
+    indexitems.first().addClass('js-active');
+    sections.first().addClass('js-active');
+
+    indexitems.on('click', e => {
+      e.preventDefault();
+      const $this = $(e.target);
+      const currentItemId = $this.data('target');
+
+      
+      indexitems.removeClass('js-active');
+      sections.removeClass('js-active');
+      
+      $this.addClass('js-active');
+      
+      sections.filter((index, el) => {
+        return $(el).data('content') === currentItemId;
+      }).addClass('js-active');
+    });
   }
 }
--- src/_scripts/main.js
+++ src/_scripts/main.js
@@ -6,9 +6,9 @@
 import 'core-js';
 import 'regenerator-runtime/runtime';
 import $ from 'jquery';
-import { Link } from '../_modules/link/link';
+import Header from '../_modules/header/header';
 
 $(() => {
-  new Link(); // Activate Link modules logic
+  new Header(); // Activate Link modules logic
   console.log('Welcome to Yeogurt!');
 });
